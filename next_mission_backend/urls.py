1cd5a61344c96b5aef8157c3c26129ad1772fbd0
mowzlisre <mowzlisre@icloud.com>
2025-06-04 07:22:37 -0400

    Media Root Change

--- next_mission_backend/urls.py
+++ next_mission_backend/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('app.urls')),
     path('api/auth/', include('users.urls')),
     path('api/v1/forum/', include('forum.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
